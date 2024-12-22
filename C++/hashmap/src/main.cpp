f84ddd09613200c2310a311c5f4628bd7630ac5a
KrunchyKrisp <m.damidavicius@gmail.com>
2024-12-22 12:59:42 +0100

    Refactor timing functions in hashmap example: change return type to size_t and calculate average execution time over multiple runs

--- C++/hashmap/src/main.cpp
+++ C++/hashmap/src/main.cpp
@@ -32,54 +32,54 @@ std::chrono::time_point<std::chrono::high_resolution_clock> start;
 
 void start_timer() { start = std::chrono::high_resolution_clock::now(); }
 
-void stop_timer() {
+size_t stop_timer() {
 	auto stop = std::chrono::high_resolution_clock::now();
 	auto duration =
 		std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-	std::cout << "Time: " << duration.count() << "ms" << std::endl;
+	return duration.count();
 }
 
-void count_words_hashmap(const std::vector<std::string> &words) {
+size_t count_words_hashmap(const std::vector<std::string> &words) {
+	start_timer();
 	hashmap<int> word_count;
-	start_timer();
 	for (const std::string &word : words) {
 		if (word.empty()) {
 			continue;
 		}
 		word_count[word]++;
 	}
-	stop_timer();
-	std::vector<std::pair<std::string, int>> items = word_count.items();
-	std::sort(items.begin(), items.end(),
-			  [](const std::pair<std::string, int> &a,
-				 const std::pair<std::string, int> &b) {
-				  return a.second > b.second;
-			  });
-	for (size_t i = 0; i < 10 && i < items.size(); i++) {
-		std::cout << items[i].first << ": " << items[i].second << std::endl;
-	}
+	return stop_timer();
+	// std::vector<std::pair<std::string, int>> items = word_count.items();
+	// std::sort(items.begin(), items.end(),
+	// 		  [](const std::pair<std::string, int> &a,
+	// 			 const std::pair<std::string, int> &b) {
+	// 			  return a.second > b.second;
+	// 		  });
+	// for (size_t i = 0; i < 10 && i < items.size(); i++) {
+	// 	std::cout << items[i].first << ": " << items[i].second << std::endl;
+	// }
 }
 
-void count_words_unordered_map(const std::vector<std::string> &words) {
-	std::unordered_map<std::string, int> word_count;
+size_t count_words_unordered_map(const std::vector<std::string> &words) {
 	start_timer();
+	std::unordered_map<std::string, int> word_count;
 	for (const std::string &word : words) {
 		if (word.empty()) {
 			continue;
 		}
 		word_count[word]++;
 	}
-	stop_timer();
-	std::vector<std::pair<std::string, int>> items(word_count.begin(),
-												   word_count.end());
-	std::sort(items.begin(), items.end(),
-			  [](const std::pair<std::string, int> &a,
-				 const std::pair<std::string, int> &b) {
-				  return a.second > b.second;
-			  });
-	for (size_t i = 0; i < 10 && i < items.size(); i++) {
-		std::cout << items[i].first << ": " << items[i].second << std::endl;
-	}
+	return stop_timer();
+	// std::vector<std::pair<std::string, int>> items(word_count.begin(),
+	// 											   word_count.end());
+	// std::sort(items.begin(), items.end(),
+	// 		  [](const std::pair<std::string, int> &a,
+	// 			 const std::pair<std::string, int> &b) {
+	// 			  return a.second > b.second;
+	// 		  });
+	// for (size_t i = 0; i < 10 && i < items.size(); i++) {
+	// 	std::cout << items[i].first << ": " << items[i].second << std::endl;
+	// }
 }
 
 int main(void) {
@@ -92,12 +92,20 @@ int main(void) {
 	std::cout << std::endl;
 
 	std::cout << "Using custom hashmap:" << std::endl;
-	count_words_hashmap(words);
+	size_t time = 0;
+	for (size_t i = 0; i < 100; i++) {
+		time += count_words_hashmap(words);
+	}
+	std::cout << "Average time: " << time / 100 << "ms" << std::endl;
 
 	std::cout << std::endl;
 
 	std::cout << "Using std::unordered_map:" << std::endl;
-	count_words_unordered_map(words);
+	time = 0;
+	for (size_t i = 0; i < 100; i++) {
+		time += count_words_hashmap(words);
+	}
+	std::cout << "Average time: " << time / 100 << "ms" << std::endl;
 
 	return 0;
 }
